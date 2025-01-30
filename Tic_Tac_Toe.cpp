5a16c59831f9795a5b559dc16d03595895f8e655
Nidhan mishra <nidhanbmishra@gmail.com>
2025-01-30 10:58:41 +0530

    "Updated too"

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -34,9 +34,14 @@ void Turn()
 
 bool win()
 {
-      return (board[0] == board[1] && board[1] == board[2]) || (board[3] == board[4] && board[4] == board[5]) || (board[6] == board[7] && board[7] == board[8]) || 
-           (board[0] == board[3] && board[3] == board[6]) || (board[1] == board[4] && board[4] == board[7]) || (board[2] == board[5] && board[5] == board[8]) ||
-           (board[0] == board[4] && board[4] == board[8]) || (board[2] == board[4] && board[4] == board[6]);
+      return (board[0] == board[1] && board[1] == board[2]) || 
+      (board[3] == board[4] && board[4] == board[5]) || 
+      (board[6] == board[7] && board[7] == board[8]) || 
+      (board[0] == board[3] && board[3] == board[6]) || 
+      (board[1] == board[4] && board[4] == board[7]) || 
+      (board[2] == board[5] && board[5] == board[8]) ||
+      (board[0] == board[4] && board[4] == board[8]) || 
+      (board[2] == board[4] && board[4] == board[6]);
         
  
 }
